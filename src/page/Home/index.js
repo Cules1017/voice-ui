571d367edbe7b313dbf81651001349619f14e96d
hieunghia <nghianguyenhieu@vnpt.vn>
2024-01-06 11:01:43 +0700

    push

--- src/page/Home/index.js
+++ src/page/Home/index.js
@@ -1,7 +1,8 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import ReactDOM from "react-dom";
-import { Button, Flex,notification, Input, Spin, Table } from 'antd';
-import { DownloadOutlined ,CopyOutlined } from '@ant-design/icons';
+import { Button, Flex,notification, Input, Spin, Table,Space } from 'antd';
+import { DownloadOutlined, CopyOutlined, SearchOutlined } from '@ant-design/icons';
+import Highlighter from 'react-highlight-words';
 
 import "./styles.css";
 import { Requests } from "../../utils/request";
@@ -45,23 +46,7 @@ function Home() {
     setListNum(saveData);
     
   }
-  const columns = [
-    {
-      title: 'port',
-      dataIndex: 'port',
-      key: 'port',
-    },
-    {
-      title: 'num',
-      dataIndex: 'num',
-      key: 'num',
-    },
-    {
-      title: 'status',
-      dataIndex: 'status',
-      key: 'status',
-    },
-  ];
+  
   const [isCopy, setIsCopy] = useState(false);
   const [copyText, setCopyText] = useState('1233333');
   const handleGetVoice = async() => {
@@ -100,11 +85,137 @@ function Home() {
       desc,
     });
   };
+  const [searchText, setSearchText] = useState('');
+  const [searchedColumn, setSearchedColumn] = useState('');
+  const searchInput = useRef(null);
+  const handleSearch = (selectedKeys, confirm, dataIndex) => {
+    confirm();
+    setSearchText(selectedKeys[0]);
+    setSearchedColumn(dataIndex);
+  };
+  const handleReset = (clearFilters) => {
+    clearFilters();
+    setSearchText('');
+  };
+  const getColumnSearchProps = (dataIndex) => ({
+    filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters, close }) => (
+      <div
+        style={{
+          padding: 8,
+        }}
+        onKeyDown={(e) => e.stopPropagation()}
+      >
+        <Input
+          ref={searchInput}
+          placeholder={`Search ${dataIndex}`}
+          value={selectedKeys[0]}
+          onChange={(e) => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+          onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
+          style={{
+            marginBottom: 8,
+            display: 'block',
+          }}
+        />
+        <Space>
+          <Button
+            type="primary"
+            onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
+            icon={<SearchOutlined />}
+            size="small"
+            style={{
+              width: 90,
+            }}
+          >
+            Search
+          </Button>
+          <Button
+            onClick={() => clearFilters && handleReset(clearFilters)}
+            size="small"
+            style={{
+              width: 90,
+            }}
+          >
+            Reset
+          </Button>
+          <Button
+            type="link"
+            size="small"
+            onClick={() => {
+              confirm({
+                closeDropdown: false,
+              });
+              setSearchText(selectedKeys[0]);
+              setSearchedColumn(dataIndex);
+            }}
+          >
+            Filter
+          </Button>
+          <Button
+            type="link"
+            size="small"
+            onClick={() => {
+              close();
+            }}
+          >
+            close
+          </Button>
+        </Space>
+      </div>
+    ),
+    filterIcon: (filtered) => (
+      <SearchOutlined
+        style={{
+          color: filtered ? '#1677ff' : undefined,
+        }}
+      />
+    ),
+    onFilter: (value, record) =>
+      record[dataIndex].toString().toLowerCase().includes(value.toLowerCase()),
+    onFilterDropdownOpenChange: (visible) => {
+      if (visible) {
+        setTimeout(() => searchInput.current?.select(), 100);
+      }
+    },
+    render: (text) =>
+      searchedColumn === dataIndex ? (
+        <Highlighter
+          highlightStyle={{
+            backgroundColor: '#ffc069',
+            padding: 0,
+          }}
+          searchWords={[searchText]}
+          autoEscape
+          textToHighlight={text ? text.toString() : ''}
+        />
+      ) : (
+        text
+      ),
+  });
+  const columns = [
+    {
+      title: 'port',
+      dataIndex: 'port',
+      key: 'port',
+      ...getColumnSearchProps('port'),
+    },
+    {
+      title: 'num',
+      dataIndex: 'num',
+      key: 'num',
+      ...getColumnSearchProps('num'),
+    },
+    {
+      title: 'status',
+      dataIndex: 'status',
+      key: 'status',
+      ...getColumnSearchProps('status'),
+    },
+  ];
     return <>
         {isLoading && <Spin size="large" fullscreen />}
         {contextHolder}
 
-<Table dataSource={listNum} columns={columns} />
+<Table style={{margin:'20px 80px'}} dataSource={listNum} columns={columns} />
       <Button onClick={handleGetPhone}> Chạy lấy số điện thoại</Button>
       <div style={{ width: "100%" ,display:"flex"}}>
         <div style={{ width: '40%',margin: '10px' }}>
